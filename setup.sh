a32c0e3a6b0113892a0c8b74e6f8f19e3fc66d30
Mar Gonzàlez-Porta <mar@ebi.ac.uk>
2014-08-04 11:32:34 +0100

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 R CMD build tviz
-R CMD check tviz_1.0.tar.gz
-R CMD INSTALL tviz_1.0.tar.gz
+R CMD check tviz_0.99.0.tar.gz
+R CMD INSTALL tviz_0.99.0.tar.gz
 
-rm tviz_1.0.tar.gz
+rm tviz_0.99.0.tar.gz
 cp tviz.Rcheck/tviz-manual.pdf .
